def1c527efcc8bef7e55921a16b9bf745864037e
sayka88 <sayka.alekperova@gmail.com>
2024-02-22 10:59:29 +0000

    1-alphabet new

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,16 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * print_alphabet - prints the alphabet in lowercase followed by a new line
- */
-void print_alphabet(void)
-{
-char letter = 'a';
+int _putchar(char c);
+void print_alphabet(void);
 
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
-}
+#endif

aa47c33a3bdb66eccfd7e8c60a2456d23931b90a
sayka88 <sayka.alekperova@gmail.com>
2024-02-22 16:03:16 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10
+ * print_alphabet_x10 - Check holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
  */
 void print_alphabet_x10(void)
 {

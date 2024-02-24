0fa3dce036a2d499ea0bbc4d434d874ca5d6e52b
sayka88 <sayka.alekperova@gmail.com>
2024-02-24 18:01:05 +0000

    9-times_table files new

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,24 +6,25 @@
 void times_table(void)
 {
 int i, j, result;
-
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 result = i * j;
-
-if (j == 0)
+if (j == 0) {
+if (result >= 10)
+{
 _putchar('0');
-else
+}
+} else
 {
 _putchar(',');
 _putchar(' ');
-
 if (result < 10)
+{
 _putchar(' ');
 }
-
+}
 if (result < 10)
 _putchar('0' + result);
 else

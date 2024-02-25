8ba4ac426e1aa936876ee828044889ad0acefd9b
sayka88 <sayka.alekperova@gmail.com>
2024-02-25 15:42:39 +0000

    100-times_table.c old

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,37 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table, starting with 0
- * @n: The number for which to print the times table
+ * print_times_table - Печатает таблицу умножения на n, начиная с 0
+ * @n: Число, для которого нужно напечатать таблицу умножения
  */
 void print_times_table(int n)
 {
 int i, j, result;
+
 if (n < 0 || n > 15)
 return;
+
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
 
-if (result < 10)
-_putchar(' ');
-else if (result < 100)
-_putchar(' ');
-
-if (result < 100)
-_putchar(' ');
-
-_putchar((result / 100) % 10 + '0');
-_putchar((result / 10) % 10 + '0');
-_putchar(result % 10 + '0');
-
-if (j < n)
+if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
+
+if (result < 100)
+_putchar(' ');
+if (result < 10)
+_putchar(' ');
+
+if (result >= 100)
+_putchar((result / 100) + '0');
+else
+_putchar(' ');
+
+if (result >= 10)
+_putchar(((result % 100) / 10) + '0');
+else
+_putchar(' ');
+
+_putchar((result % 10) + '0');
 }
 _putchar('\n');
 }

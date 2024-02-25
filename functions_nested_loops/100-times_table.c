7e80f4f8f4cc0535dd288f0e5f46eef35eca9e5e
sayka88 <sayka.alekperova@gmail.com>
2024-02-25 07:12:55 +0000

    100-times_table.c file

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,35 +6,33 @@
  */
 void print_times_table(int n)
 {
-int i, j, result;
 if (n < 0 || n > 15)
 return;
+
+int i, j, result;
+
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
 
-if (j > 0)
-_putchar(',');
-
 if (result < 10)
-{
 _putchar(' ');
-_putchar(' ');
-_putchar(result + '0');
-}
 else if (result < 100)
-{
 _putchar(' ');
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-}
-else
+
+if (result < 100)
+_putchar(' ');
+
+_putchar((result / 100) % 10 + '0');
+_putchar((result / 10) % 10 + '0');
+_putchar(result % 10 + '0');
+
+if (j < n)
 {
-_putchar((result / 100) + '0');
-_putchar(((result / 10) % 10) + '0');
-_putchar((result % 10) + '0');
+_putchar(',');
+_putchar(' ');
 }
 }
 _putchar('\n');

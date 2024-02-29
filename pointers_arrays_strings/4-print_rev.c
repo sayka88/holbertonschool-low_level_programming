ec1f45feaebc85a7fac09754eb182ce10d913518
sayka88 <sayka.alekperova@gmail.com>
2024-02-29 11:41:36 +0000

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: Pointer to the string
+ */
+void print_rev(char *s)
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+        ;
+        for (i--; i >= 0; i--)
+        _putchar(s[i]);
+    _putchar('\n');
+
+}

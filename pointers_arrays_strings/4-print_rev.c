2a7ef4665a38c9c08c8fad7378d21c3014cfec0c
sayka88 <sayka.alekperova@gmail.com>
2024-02-29 11:43:34 +0000

    4-print_rev.c new

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
  */
 void print_rev(char *s)
 {
-    int i;
-    for (i = 0; s[i] != '\0'; i++)
-        ;
-        for (i--; i >= 0; i--)
-        _putchar(s[i]);
-    _putchar('\n');
+int i;
+for (i = 0; s[i] != '\0'; i++)
+	;
+	for (i--; i >= 0; i--)
+	_putchar(s[i]);
+_putchar('\n');
 
 }

9138096c65de835781f0d702673d64f5f4955f82
sayka88 <sayka.alekperova@gmail.com>
2024-03-02 18:57:52 +0000

    3-main.c

---
+++ structures_typedef/3-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - основная функция
+ *
+ * Return: всегда возвращает 0 (успех)
+ */
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+
+	return (0);
+}

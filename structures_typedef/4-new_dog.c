f543fbfe225d374f39b1eef8535e4ac8f465aa4c
sayka88 <sayka.alekperova@gmail.com>
2024-03-03 08:24:05 +0000

    4-new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,33 +14,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
+    dog_t *new_dog;
+    size_t name_len, owner_len;
 
-if (name == NULL || owner == NULL)
-	return (NULL);
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-	return (NULL);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
 
-new_dog->name = malloc(strlen(name) + 1);
-if (new_dog->name == NULL)
-{
-	free(new_dog);
-	return (NULL);
-}
-strcpy(new_dog->name, name);
+    name_len = strlen(name);
+    new_dog->name = malloc(name_len + 1);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+    memcpy(new_dog->name, name, name_len + 1);
 
-new_dog->owner = malloc(strlen(owner) + 1);
-if (new_dog->owner == NULL)
-{
-	free(new_dog->name);
-	free(new_dog);
-	return (NULL);
-}
-strcpy(new_dog->owner, owner);
+    owner_len = strlen(owner);
+    new_dog->owner = malloc(owner_len + 1);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
+    memcpy(new_dog->owner, owner, owner_len + 1);
 
-new_dog->age = age;
+    new_dog->age = age;
 
-return (new_dog);
+    return (new_dog);
 }

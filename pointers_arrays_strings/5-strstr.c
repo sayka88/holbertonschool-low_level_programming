4dea17c118f692d36c2fc77f383ba5aa6ea36452
sayka88 <sayka.alekperova@gmail.com>
2024-03-05 11:54:19 +0000

    5-strstr new

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: substring to locate
  *
- * Return: Always 0.
+ * Return: pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {

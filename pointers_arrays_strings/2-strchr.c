d7100e6c0ad313168d96fe777d906535c74b061e
sayka88 <sayka.alekperova@gmail.com>
2024-03-05 08:43:21 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,13 @@
  */
 
 char *_strchr(char *s, char c)
-{
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
 
-	return (NULL);
+{
+    while (*s)
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
+    return NULL;
 }

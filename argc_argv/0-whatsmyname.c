1e933c80f07a77a4becfefa42d71fd231d0fc682
sayka88 <sayka.alekperova@gmail.com>
2024-03-08 15:07:25 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,9 +10,12 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", argv[0]);
+#include <stdio.h>
 
-	return (0);
+int main(int argc, char *argv[]) {
+	if (argc >= 1) {
+		printf("%s\n", argv[0]);
+	}
+
+	return 0;
 }

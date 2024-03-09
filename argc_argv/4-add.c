9a88c71d0f2544e3a0d89e137cbbc9a925d16ef9
sayka88 <sayka.alekperova@gmail.com>
2024-03-09 13:51:16 +0000

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_positive_number - Checks if a string is a positive number
+ * @str: String to check
+ * Return: 1 if the string is a positive number, 0 otherwise
+ */
+
+int is_positive_number(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - A program that adds positive numbers
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}

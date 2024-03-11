4176d18cb1d298efceda46ed40ad45a6769e01ac
sayka88 <sayka.alekperova@gmail.com>
2024-03-11 07:50:54 +0000

    0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,32 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - Creates an array of chars
+ *and initializes it with a specific char
+ * @size: The size of the array
+ * @c: The character to initialize the array with
  *
- * Return: Nothing.
+ * Return: Pointer to the array, or NULL if it fails
  */
-char *create_array(unsigned int size, char c)
 
+char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *array;
+
 	if (size == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
-array = malloc(size * sizeof(char));
 
-if (array == NULL)
-{
-	return (NULL);
-}
-for (i = 0; i < size; i++)
-{
-	array[i] = c;
-}
-return (array);
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
 }

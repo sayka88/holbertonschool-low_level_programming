21c21c34205c5c7272be7896daeac8b8228b8368
sayka88 <sayka.alekperova@gmail.com>
2024-03-12 05:36:28 +0000

    2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,37 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * str_concat - Concatenates two strings.
- * @s1: The first string
- * @s2: The second string
+ * @s1: First input string.
+ * @s2: Second input string.
  *
- * Return: A pointer to the concatenated string
- * or NULL if memory allocation fails
+ * Return: Pointer to the newly allocated concatenated string,
+ *         or NULL on failure.
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-	size_t i, j;
+	char *result;
+	int len1 = 0, len2 = 0, i, j;
 
-	char *concatenated = malloc(len1 + len2 + 1);
-	if (concatenated == NULL)
-	{
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	result = malloc((len1 + len2 + 1) * sizeof(char));
+	if (result == NULL)
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
-		concatenated[i] = s1[i];
-	}
-
+		result[i] = s1[i];
 	for (j = 0; j < len2; j++)
-	{
-		concatenated[i + j] = s2[j];
-	}
+		result[i + j] = s2[j];
 
-	concatenated[len1 + len2] = '\0';
-
-	return (concatenated);
+	result[i + j] = '\0';
+	return (result);
 }

aa88266917957b1c36a7f28dc46b6d8a08093945
sayka88 <sayka.alekperova@gmail.com>
2024-03-13 08:52:08 +0000

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - get len, malloc * len
+ * loop size of len to insert min++ into new arr
+ * @min: min input
+ * @max: max input
+ * Return: pointer to new arr of ints
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size, i;
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
+	
+	return (arr);
+}

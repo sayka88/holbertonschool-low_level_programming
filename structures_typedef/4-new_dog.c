b527bfaed6298ac390e34d97ef8acd1fab8cf528
sayka88 <sayka.alekperova@gmail.com>
2024-03-14 10:16:21 +0000

    4-new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,25 +19,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = strdup(name);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
 
-	owner_copy = NULL;
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = strdup(owner);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
-
-	/* Fill in the dog structure */
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;

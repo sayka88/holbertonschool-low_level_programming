b7ca55ada7e4c45c89ca2c5badcd805176f6f99c
sayka88 <sayka.alekperova@gmail.com>
2024-03-22 13:18:33 +0000

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}

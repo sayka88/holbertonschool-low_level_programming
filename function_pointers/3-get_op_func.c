74d0812fedc2a4a909a5322694db02b962e9caa6
sayka88 <sayka.alekperova@gmail.com>
2024-03-22 16:49:59 +0000

    3-calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function to
@@ -23,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s && *(ops[i].op + 1) == '\0')
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}

86c5ad0b6a17eb60907dbb1d3e5569ca6fa84e32
sayka88 <sayka.alekperova@gmail.com>
2024-03-24 18:03:49 +0000

    100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,17 @@
-	global main
+section .data
+message db "Hello, World", 10
 
-	section .text
+section .text
+global main
 main:
+	; write syscall
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
 	mov rdx, 13
 	syscall
 
+	; exit syscall
 	mov eax, 60
 	xor rdi, rdi
 	syscall
-
-message:
-	db "Hello, World", 13

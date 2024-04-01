af28c2872b7d65c257612474b6509d0b13675fc4
sayka88 <sayka.alekperova@gmail.com>
2024-04-01 08:02:55 +0000

    add test folder

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function prototype */
+size_t print_list(const list_t *h);
+
+#endif
+

738f259c84cab8b4cffaaf280fad22f5a3684172
sayka88 <sayka.alekperova@gmail.com>
2024-04-02 13:17:15 +0000

    updated

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) print_before_main();
-
 /**
 * print_before_main - Prints a message before main function is executed
 */
-void print_before_main()
+void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
 }

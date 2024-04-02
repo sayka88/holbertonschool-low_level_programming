37f6b34d46d00fb45d99b8efd153540cd9a28941
sayka88 <sayka.alekperova@gmail.com>
2024-04-02 15:23:04 +0000

    corrections

--- singly_linked_lists/test/100-first.c
+++ singly_linked_lists/test/100-first.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/* Using constructors in C */
+void main_constructor(void) __attribute__((constructor));
 /**
-* print_before_main - Prints a message before main function is executed
-*/
-void print_before_main(void)
+ * main_constructor - A constructor function that prints a message
+ * before main runs
+ * Return: Nothing
+ */
+void main_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n);
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }

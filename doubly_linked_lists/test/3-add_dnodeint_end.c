f54ab9d8c90fa678383018e89a8f2ea475eddc2b
sayka88 <sayka.alekperova@gmail.com>
2024-04-03 08:50:30 +0000

    modified 3-add_dnodeint_end.c

--- doubly_linked_lists/test/3-add_dnodeint_end.c
+++ doubly_linked_lists/test/3-add_dnodeint_end.c
@@ -12,7 +12,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *temp = *head;
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -35,3 +34,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	return (new_node);
 }
+

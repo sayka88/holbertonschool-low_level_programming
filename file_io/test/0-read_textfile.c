df78c1eb1a67bf2bd79f46a262c2e5496cb01acc
sayka88 <sayka.alekperova@gmail.com>
2024-04-08 06:30:32 +0000

    test folder modified

--- file_io/test/0-read_textfile.c
+++ file_io/test/0-read_textfile.c
@@ -3,6 +3,13 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ **/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, rd;

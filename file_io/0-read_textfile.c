f51b0c3074de4e2f71809c5f6a9f7fb92a85a5c2
sayka88 <sayka.alekperova@gmail.com>
2024-04-08 06:14:29 +0000

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters) {
+	int fd, rd;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf) {
+		close(fd);
+		return (0);
+	}
+
+	rd = read(fd, buf, letters);
+	if (rd == -1) {
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	write(STDOUT_FILENO, buf, rd);
+
+	free(buf);
+	close(fd);
+
+	return (rd);
+}

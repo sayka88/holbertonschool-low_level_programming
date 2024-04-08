4ea8b61715a1f34e56fd8df759e410599ebf0ceb
sayka88 <sayka.alekperova@gmail.com>
2024-04-08 06:25:40 +0000

    main.h modified

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,46 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ **/
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-#endif
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	write(STDOUT_FILENO, buf, rd);
+
+	free(buf);
+	close(fd);
+
+	return (rd);
+}
